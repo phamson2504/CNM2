18c0ebe2c58d92d77665e711d70ec63e1f9e594c
phamson2504 <78399005+phamson2504@users.noreply.github.com>
2021-12-22 02:06:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ const uploadImage = multer({
   limits:{fileSize:2000000},
 })
 const s3= new AWS.S3({
-  accessKeyId:'AKIAV76VXQMSB2TFD3FR',
-  secretAccessKey:'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',
+  accessKeyId:'',
+  secretAccessKey:'',
 })
 //tao server io
 const http = require('http');
@@ -103,7 +103,7 @@ io.on('connection', (socket) => {
       }
     }
     
-    setTimeout(intervalFunc, 11500, 'funky');
+    setTimeout(intervalFunc, 12500, 'funky');
    })
   const storage1=multer.memoryStorage({
     destination(req,file,callback){
@@ -183,7 +183,7 @@ var db= mysql.createConnection({
   // user: "root",
   // password: "nhatban1",
   // database: "baitap"
-  host: "baitapnhomcnm.crkxbwz5flrz.ap-southeast-1.rds.amazonaws.com",
+  host: "",
   port: "3306",
   user: "admin",
   password: "Nhatban1",
@@ -200,7 +200,7 @@ var db= mysql.createConnection({
   // user: "root",
   // password: "nhatban1",
   // database: "baitap"
-  host: "baitapnhomcnm.crkxbwz5flrz.ap-southeast-1.rds.amazonaws.com",
+  host: "",
   port: "3306",
   user: "admin",
   password: "Nhatban1",
